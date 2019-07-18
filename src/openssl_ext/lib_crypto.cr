007400445eb8e813629379392336e4e57ee284f2
Kevin <root@unixs.org>
2019-07-19 03:39:04 +0800

    给LibCrypto::X509添加了C binding

--- src/openssl_ext/lib_crypto.cr
+++ src/openssl_ext/lib_crypto.cr
@@ -193,6 +193,9 @@ lib LibCrypto
 
   BIO_CTRL_RESET = 1
 
+  NID_ext_key_usage = 126
+  NID_key_usage     =  83
+
   NID_rsaEncryption        =   6
   NID_dsa                  = 116
   NID_X9_62_id_ecPublicKey = 408
@@ -209,6 +212,7 @@ lib LibCrypto
   type BnCtx = Void*
   type BnBlinding = Void*
 
+  alias ASN1_INTEGER = Void*
   alias ASN1_TIME = Void*
 
   fun obj_txt2nid = OBJ_txt2nid(s : UInt8*) : Int32
@@ -297,9 +301,42 @@ lib LibCrypto
   # Adding x509 Capabilities
   fun x509_name_print_ex = X509_NAME_print_ex(bio : Bio*, name : X509_NAME, indent : Int32, flags : LibC::ULong) : LibC::Int
   fun x509_gmtime_adj = X509_gmtime_adj(t : ASN1_TIME, adj : Int64) : ASN1_TIME
+  fun x509_store_ctx_get_error = X509_STORE_CTX_get_error(store : X509_STORE_CTX) : Int32
+  fun x509_store_ctx_get_current_cert = X509_STORE_CTX_get_current_cert(store : X509_STORE_CTX) : X509
+
   fun pem_read_bio_x509 = PEM_read_bio_X509(bp : Bio*, x : X509*, cb : (LibC::Char*, LibC::Int, LibC::Int, Void* -> LibC::Int), u : Void*) : X509
   fun pem_write_bio_x509 = PEM_write_bio_X509(bp : Bio*, x : X509) : LibC::Int
-  fun x509_get_public_key = X509_get_pubkey(x : X509) : EvpPKey*
   fun evp_sign_final = EVP_SignFinal(ctx : EVP_MD_CTX, md : UInt8*, s : LibC::UInt*, pkey : EvpPKey*) : LibC::Int
   fun evp_verify_final = EVP_VerifyFinal(ctx : EVP_MD_CTX, sigbuf : UInt8*, siglen : LibC::UInt, pkey : EvpPKey*) : LibC::Int
+
+  fun x509_digest = X509_digest(x509 : X509, type : EVP_MD, md : UInt8*, len : UInt32*) : Int32
+  fun x509_get_public_key = X509_get_pubkey(x509 : X509) : EvpPKey*
+  fun x509_get_subject_name = X509_get_subject_name(x509 : X509) : X509_NAME
+  fun x509_get_serialnumber = X509_get_serialNumber(x509 : X509) : ASN1_INTEGER
+  fun x509_set_version = X509_set_version(x509 : X509, version : Int64) : Int32
+  fun x509_set_pubic_key = X509_set_pubkey(x509 : X509, pkey : EvpPKey*) : Int32
+  fun x509_set_notbefore = X509_set1_notBefore(x509 : X509, tm : ASN1_TIME) : Int32
+  fun x509_set_notafter = X509_set1_notAfter(x509 : X509, tm : ASN1_TIME) : Int32
+  fun x509_set_issuer_name = X509_set_issuer_name(x509 : X509, name : X509_NAME) : Int32
+  fun x509_sign = X509_sign(x509 : X509, pkey : EvpPKey*, md : EVP_MD) : Int32
+  fun x509_verify = X509_verify(x509 : X509, pkey : EvpPKey*) : Int32
+
+  fun x509_extension_free = X509_EXTENSION_free(ex : X509_EXTENSION)
+
+  struct X509V3_CTX
+    flags : Int32
+    issuer_cert : Void*
+    subject_cert : Void*
+    subject_req : Void*
+    crl : Void*
+    db_meth : Void*
+    db : Void*
+  end
+
+  alias X509_REQ = Void*
+  alias X509_CRL = Void*
+
+  fun x509v3_set_ctx = X509V3_set_ctx(ctx : X509V3_CTX*, issuer : X509, subj : X509, req : X509_REQ,
+                                      crl : X509_CRL, flags : Int32)
+  fun x509v3_ext_conf_nid = X509V3_EXT_conf_nid(conf : Void*, ctx : X509V3_CTX*, ext_nid : Int32, value : UInt8*) : X509_EXTENSION
 end
