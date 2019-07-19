4477df69f485f6bc46082c9a976dc12359b35cab
Kevin <root@unixs.org>
2019-07-20 02:48:52 +0800

    修复了bug

--- src/openssl_ext/x509/extension_factory.cr
+++ src/openssl_ext/x509/extension_factory.cr
@@ -13,18 +13,16 @@ module OpenSSL::X509
     end
 
     def create_extension(oid : String, value : String, critical = false) : Extension
-      LibCrypto.x509v3_set_ctx(self, self.issuer_cert, self.subject_cert, nil, nil, 0)
+      LibCrypto.x509v3_set_ctx(self, @issuer_cert, @subject_cert, nil, nil, 0)
       Extension.new(self, oid, value, critical)
     end
 
     def subject_certificate=(cert : Certificate)
-      self.subject_cert = cert
-      @ctx.subject_cert = cert.as Void*
+      @subject_cert = cert
     end
 
     def issuer_certificate=(cert : Certificate)
-      self.issuer_cert = cert
-      @ctx.issuer_cert = cert.as Void*
+      @issuer_cert = cert
     end
 
     def finalize
