66cd74417b931352d6f07e9ebb36592aeee5be27
Stephen von Takach <steve@vontaka.ch>
2021-01-29 12:54:41 +1100

    fix: restore 0.35.1 support

--- src/openssl_ext/digest/digest.cr
+++ src/openssl_ext/digest/digest.cr
@@ -1,26 +1,24 @@
 require "digest"
 require "digest/*"
 
-class Digest
-  macro def_digest_classes(names)
-    {% for name in names %}
-      class {{name.id}} < ::OpenSSL::Digest
-        extend ClassMethods
+macro def_digest_classes(names)
+  {% for name in names %}
+    class Digest::{{name.id}} < ::OpenSSL::Digest
+      extend ClassMethods
 
-        def initialize
-          super("{{name.id}}")
-        end
-
-        protected def initialize(ctx : LibCrypto::EVP_MD_CTX)
-          super("{{name.id}}", ctx)
-        end
-
-        def dup
-          self.class.new(dup_ctx)
-        end
+      def initialize
+        super("{{name.id}}")
       end
-    {% end %}
-  end
 
-  def_digest_classes %w(DSS DSS1 MD2 MD4 MD5 MDC2 RIPEMD160 SHA SHA1 SHA224 SHA256 SHA384 SHA512)
+      protected def initialize(ctx : LibCrypto::EVP_MD_CTX)
+        super("{{name.id}}", ctx)
+      end
+
+      def dup
+        self.class.new(dup_ctx)
+      end
+    end
+  {% end %}
 end
+
+def_digest_classes %w(DSS DSS1 MD2 MD4 MD5 MDC2 RIPEMD160 SHA SHA1 SHA224 SHA256 SHA384 SHA512)
