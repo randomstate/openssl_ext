3ed7a03a735ffc88ceb1e0cafd02bc1081b1993d
Stephen von Takach <steve@place.technology>
2023-02-22 21:33:56 +1100

    fix(bio): resolve issues using BioMethodCtrl from std lib

--- src/openssl_ext/bio/bio.cr
+++ src/openssl_ext/bio/bio.cr
@@ -3,8 +3,33 @@ module OpenSSL
 end
 
 class OpenSSL::GETS_BIO
+  BIO_C_FILE_TELL = 133
+  BIO_C_FILE_SEEK = 128
+
   GETS_BIO = begin
     crystal_bio = OpenSSL::BIO::CRYSTAL_BIO
+
+    ctrl = LibCrypto::BioMethodCtrl.new do |bio, cmd, _num, _ptr|
+      val = case cmd
+            when LibCrypto::CTRL_FLUSH
+              io = Box(IO).unbox(BIO.get_data(bio))
+              io.flush
+              1
+            when LibCrypto::CTRL_PUSH, LibCrypto::CTRL_POP, LibCrypto::CTRL_EOF
+              0
+            when BIO_C_FILE_TELL, BIO_C_FILE_SEEK
+              0
+            when LibCrypto::CTRL_SET_KTLS_SEND
+              0
+            when LibCrypto::CTRL_GET_KTLS_SEND, LibCrypto::CTRL_GET_KTLS_RECV
+              0
+            else
+              STDERR.puts "WARNING: Unsupported BIO ctrl call (#{cmd})"
+              0
+            end
+      LibCrypto::Long.new(val)
+    end
+
     bgets = LibCrypto::BioMethodGets.new do |bio, buffer, len|
       io = Box(IO).unbox(BIO.get_data(bio))
       io.flush
@@ -23,9 +48,13 @@ class OpenSSL::GETS_BIO
         bytes
       end
     end
+    # use our version of ctrl to avoid warnings
+    # is also more performant than the standard library version
     {% if compare_versions(LibCrypto::OPENSSL_VERSION, "1.1.0") >= 0 %}
+      LibCrypto.BIO_meth_set_ctrl(crystal_bio, ctrl)
       LibCrypto.BIO_meth_set_gets(crystal_bio, bgets)
     {% else %}
+      crystal_bio.value.ctrl = ctrl
       crystal_bio.value.bgets = bgets
     {% end %}
     crystal_bio
